854e721ba1b54c5352bfb265b98c2e609b066672
Dev Goyal <devgoyal9031@gmail.com>
2022-12-30 20:26:39 +0530

    Completed Datasets formation and PCA function

--- MyStock.py
+++ MyStock.py
@@ -1,10 +1,14 @@
-
-import yfinance as yf
 import pandas as pd
 import numpy as np
 import utils
 from sklearn.decomposition import PCA
+from sklearn.preprocessing import StandardScaler
+import requests
+import time
+from functools import reduce
 
+LIMIT = 10
+OFFSET = 0
 
 dataset = "Company Names and Ticker Symbols/yahootickers.xlsx"
 cols = ["Ticker","Name","Exchange","Category Name"]
@@ -23,18 +27,33 @@ def get_data(dataset, start, end, cols):
     df_companies = pd.read_excel(dataset,sheet_name= "Stock",skiprows=3,usecols=cols)
     df_companies.dropna(inplace=True)
     stocks = []
-    for i in range(0,len(df_companies),100):
-        df_stock = yf.download(df_companies["Ticker"].tolist()[i:i+100],start=start,end=end)
-        stocks.append(df_stock)
+    # e = len(df_companies)
+    e = 5
+    for i in range(0,e//5, 5):
+        for j in range(i, i+5):
+            symbol = df_companies['Ticker'].to_list()[j]
+            print(symbol)
+            url = f'https://www.alphavantage.co/query?function=TIME_SERIES_MONTHLY&symbol={symbol}&apikey=S1T4GAWWNLWYKAMF'
+            r = requests.get(url)
+            data = r.json()
+            df = pd.DataFrame.from_dict(data['Monthly Time Series'], orient='index')
+            df.rename(columns={"4. close": symbol}, inplace=True)
+            df.drop(columns=["1. open","2. high","3. low","5. volume"], inplace=True)
+            df['date'] = df.index
+            df['date'] = pd.to_datetime(df['date'])
+            df = df[(df['date'] >= start) & (df['date'] <= end)]
+            stocks.append(df)
+
+            
+        # time.sleep(65)
         # save to csv
         # df_stock.to_csv("Stocks/{}.csv".format(i))
-    df_stock = pd.concat(stocks)
-    df_stock = df_stock[["Adj Close"]]
-    df_stock.columns = df_stock.columns.droplevel(0)
-    print(df_stock.head())
+    df_stock = reduce(lambda left,right: pd.merge(left,right,on='date'), stocks)
+    
+
     return df_companies, df_stock
 
-def perform_pca(X_train, Y_train, n_components):
+def perform_pca(X_train, Y_train):
     '''
     @param X_train: training features
     @param Y_train: training labels
@@ -45,13 +64,34 @@ def perform_pca(X_train, Y_train, n_components):
     perform pca on training data
 
     '''
-    pca = PCA(n_components=n_components)
-    X_train_pca = pca.fit_transform(X_train)
-    Y_train_pca = pca.fit_transform(Y_train)
-    return pca, X_train_pca, Y_train_pca
+    pca = PCA(0.95)
+    X = X_train.loc[:, X_train.columns != "date"].values()
+    x = StandardScaler().fit_transform(X)
+    Y = Y_train.loc[:, "Adj Close"].values()
+    pca.fit(x)
+    n_components = pca.n_components_
 
-df_companies, df_stocks = get_data(dataset, "2019-01-01", "2020-01-01", cols)
+    X_train_pca = pca.transform(x)
 
+    x_df = pd.DataFrame(data = X_train_pca, columns = ["PC{}".format(i) for i in range(1,n_components+1)])
+
+    to_return  = pd.concat([Y_train["Adj Close"],x_df], axis=1)
+
+    return to_return
+
+start_date = "2018-01-01"
+end_date = "2020-01-01"
+df_companies, df_stocks = get_data(dataset, start=start_date, end=end_date, cols=cols)
+df_mei = utils.get_mei(start=start_date, end=end_date, limit=LIMIT, offset=OFFSET)
+OFFSET += LIMIT
+df = utils.combine_datasets(df_mei)
+
+
+print(df.head())
+
+print(df_companies.head())
+
+print(df_stocks.head())
 
 
 
--- utils.py
+++ utils.py
@@ -1,12 +1,15 @@
 import pandas as pd
 from full_fred.fred import Fred
 import numpy as np
+from functools import reduce
 
 
-LIMIT = 2
+
+LIMIT = 10
 fred = Fred("./fred_api_key.txt")
+OFFSET = 0
 
-def get_mei(start=None,end=None):
+def get_mei(start=None,end=None, limit=LIMIT, offset=OFFSET):
     '''
     @param start: start date
     @param end: end date
@@ -14,16 +17,36 @@ def get_mei(start=None,end=None):
     @return to_return: list of dataframes of mei
     get Main Economic Indicators from fred
     '''
-    if start and end:
+
+    if start:
         fred.observation_start = start
+    if end:
         fred.observation_end = end
-    r = fred.get_series_matching_tags(["mei"], limit=LIMIT)
+    r = fred.get_series_matching_tags(["mei","monthly","usa"], limit=limit, offset=offset)
     datasets = r['seriess']
-    to_return  = [fred.get_series_df(i['id']) for i in datasets]
+    to_return  = {i['id'] : fred.get_series_df(i['id']) for i in datasets}
     return to_return
 
+def combine_datasets(data, on='date', exclude=["realtime_start","realtime_end"]):
+    '''
+    @param data: list of dataframes
+    
+    @return df: dataframe of all dataframes
+    '''
+    datasets = []
+    for key in data:
+        if "date" not in data[key].columns:
+            continue
+        data[key].rename(columns={"value": key}, inplace=True)
+        data[key].drop(columns=exclude, inplace=True, errors = "ignore")
+        datasets.append(data[key])
+    
+    
+    df = reduce(lambda left,right: pd.merge(left,right,on=on), datasets)
+    return df
 
-# get_mei()
+
+
 
 
 
